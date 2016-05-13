4aa666a35dc30b7b912ece51249e011a46feddd9
Ivano Bilenchi <ivanobilenchi@gmail.com>
2016-05-13 15:40:31 +0200

    Add lazy property synthesis directives

--- MicroReasoner/SMRPreprocessor.h
+++ MicroReasoner/SMRPreprocessor.h
@@ -9,12 +9,48 @@
 #ifndef SMRPreprocessor_h
 #define SMRPreprocessor_h
 
-// Pseudo-abstract class convenience macros.
-
+/**
+ * Use this directive to mark method implementations that should be overridden
+ * by concrete subclasses.
+ */
 #define ABSTRACT_METHOD {\
 @throw [NSException exceptionWithName:NSInternalInconsistencyException \
 reason:@"This method should be overridden in a subclass." \
 userInfo:nil]; \
+}
+
+/**
+ * Use this directive in place of @synthesize to automatically create a
+ * lazy getter for the specified property. Example syntax:
+ *
+ * SYNTHESIZE_LAZY(NSMutableString, myMutableString) {
+ *     return [NSMutableString stringWithString:@"my mutable string"];
+ * }
+ *
+ * @param type The type of the property.
+ * @param name The name of the property.
+ */
+#define SYNTHESIZE_LAZY(type, name) \
+@synthesize name = _##name; \
+- (type *)name { \
+if (_##name == nil) { _##name = [self __##name##LazyInit]; } \
+return _##name; \
+} \
+- (type *)__##name##LazyInit
+
+/**
+ * Use this directive in place of @synthesize to automatically create a
+ * lazy getter for the specified property. The getter calls the default
+ * 'init' constructor.
+ *
+ * @param type The type of the property.
+ * @param name The name of the property.
+ */
+#define SYNTHESIZE_LAZY_INIT(type, name) \
+@synthesize name = _##name; \
+- (type *)name { \
+if (_##name == nil) { _##name = [[type alloc] init]; } \
+return _##name; \
 }
 
 #endif /* SMRPreprocessor_h */
