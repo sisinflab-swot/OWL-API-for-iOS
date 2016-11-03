459528e6793a3af343add7bd6f0a1b7bb5ae54fa
Ivano Bilenchi <ivanobilenchi@gmail.com>
2016-11-03 18:38:11 +0100

    Do not automatically attempt to install pkg-config

--- librdf/build/build.sh
+++ librdf/build/build.sh
@@ -9,13 +9,7 @@ set -o errexit
 # Check for pkg-config and brew, if needed and possible
 if [ 'x' == $(which pkg-config)'x' ]; then
     echo "pkg-config is required in order to compile librdf."
-	if [ 'x' == $(which brew)'x' ]; then
-		echo "Attempted to install pkg-config via Homebrew, but it is not installed. Please install pkg-config or Homebrew."
-		exit 1
-	fi
-	
-	echo "Installing pkg-config"
-	brew install pkg-config
+	exit 1
 fi
 
 # Cross compile
