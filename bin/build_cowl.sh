9425a80855437d11647da32c380bf664ca745482
Ivano Bilenchi <ivanobilenchi@gmail.com>
2020-11-06 20:05:38 +0100

    Improve parametrization of build script

--- bin/build_cowl.sh
+++ bin/build_cowl.sh
@@ -2,15 +2,19 @@
 
 # Configuration
 
+CMAKE_DIR="$(dirname "${0}")/../lib/cowl"
+CMAKE_TARGET="cowl-static"
+LIB_NAME="libcowl.a"
+
 MACOS_ARCHS="x86_64"
 MACOS_BUILD_DIR="cmake-build-macos"
-MACOS_DEPLOYMENT_TARGET=10.11
+MACOS_DEPLOYMENT_TARGET="10.11"
 
-IOS_ARCHS="arm64;arm64e"
+IOS_ARCHS="armv7;armv7s;arm64;arm64e"
 SIM_ARCHS="x86_64"
 IOS_BUILD_DIR="cmake-build-ios"
 SIM_BUILD_DIR="cmake-build-sim"
-IOS_DEPLOYMENT_TARGET=9.0
+IOS_DEPLOYMENT_TARGET="9.0"
 
 ACTION="${1:-build}"
 CONFIG="${CONFIGURATION:-Release}"
@@ -31,33 +35,34 @@ function cmake {
     command env -i PATH="/usr/local/opt/flex/bin:/usr/local/opt/bison/bin:${PATH}" cmake "${@}"
 }
 
+function clean {
+    rm -rf "${MACOS_BUILD_DIR}" "${IOS_BUILD_DIR}" "${SIM_BUILD_DIR}" "${OUT_DIR}"
+}
+
 # Start build
 
-echo "Printing env..."
-printenv
-
-cd "$(dirname "${0}")/../lib/cowl"
+cd "${CMAKE_DIR}"
 
 COMMIT_SHA="$(git rev-parse --short HEAD)"
 CONFIG_FLAG="${OUT_DIR}/${COMMIT_SHA}_${CONFIG}"
 
 if [ "${ACTION}" = "clean" ]; then
-    rm -rf "${MACOS_BUILD_DIR}" "${IOS_BUILD_DIR}" "${SIM_BUILD_DIR}" "${OUT_DIR}"
+    clean
     exit 0
 fi
 
 if [ ! -f  "${CONFIG_FLAG}" ]; then
-    rm -rf "${MACOS_BUILD_DIR}" "${IOS_BUILD_DIR}" "${SIM_BUILD_DIR}" "${OUT_DIR}"
+    clean
 
     # macOS library
     cmake -B "${MACOS_BUILD_DIR}" \
         -DCMAKE_OSX_ARCHITECTURES="${MACOS_ARCHS}" \
         -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOS_DEPLOYMENT_TARGET}"
 
-    cmake --build "${MACOS_BUILD_DIR}" --target cowl-static --config "${CONFIG}" --parallel
+    cmake --build "${MACOS_BUILD_DIR}" --target "${CMAKE_TARGET}" --config "${CONFIG}" --parallel
 
     mkdir -p "${MACOS_OUT_DIR}"
-    mv "${MACOS_BUILD_DIR}/libcowl.a" "${MACOS_OUT_DIR}"
+    mv "${MACOS_BUILD_DIR}/${LIB_NAME}" "${MACOS_OUT_DIR}"
 
     # iOS library
     cmake -B "${IOS_BUILD_DIR}" \
@@ -67,7 +72,7 @@ if [ ! -f  "${CONFIG_FLAG}" ]; then
         -DCMAKE_OSX_ARCHITECTURES="${IOS_ARCHS}" \
         -DCMAKE_C_FLAGS='-fembed-bitcode'
 
-    cmake --build "${IOS_BUILD_DIR}" --target cowl-static --config "${CONFIG}" --parallel
+    cmake --build "${IOS_BUILD_DIR}" --target "${CMAKE_TARGET}" --config "${CONFIG}" --parallel
 
     # Simulator library
     cmake -B "${SIM_BUILD_DIR}" \
@@ -77,11 +82,11 @@ if [ ! -f  "${CONFIG_FLAG}" ]; then
         -DCMAKE_OSX_ARCHITECTURES="${SIM_ARCHS}" \
         -DCMAKE_C_FLAGS='-fembed-bitcode'
 
-    cmake --build "${SIM_BUILD_DIR}" --target cowl-static --config "${CONFIG}" --parallel
+    cmake --build "${SIM_BUILD_DIR}" --target "${CMAKE_TARGET}" --config "${CONFIG}" --parallel
 
     mkdir -p "${IOS_OUT_DIR}"
-    lipo -create "${IOS_BUILD_DIR}/libcowl.a" "${SIM_BUILD_DIR}/libcowl.a" \
-         -output "${IOS_OUT_DIR}/libcowl.a"
+    lipo -create "${IOS_BUILD_DIR}/${LIB_NAME}" "${SIM_BUILD_DIR}/${LIB_NAME}" \
+         -output "${IOS_OUT_DIR}/${LIB_NAME}"
 
     # Headers
     mv "${IOS_BUILD_DIR}/include" "${HEADERS_OUT_DIR}"
